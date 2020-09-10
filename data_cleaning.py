7c8dee39139fee8be3fc976d615cc8f255909ad8
Mel <55166494+melyndabillings@users.noreply.github.com>
2020-09-10 00:32:23 -0500

    data cleaned
    salary cleaned

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,4 @@
-# -*- co=ng: utf-8 -*-.,.
 """
-Created on Sat Sep  5 13:03:47 2020
-
 @author: melyndabillings
 """
 
@@ -9,19 +6,16 @@ import pandas as pd
 
 df = pd.read_csv('glassdoor_jobs.csv')
 
-#salary parsing 
-
-df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
-df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
+#parsing of salary 
 
 df = df[df['Salary Estimate'] != '-1']
 salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
-minus_Kd = salary.apply(lambda x: x.replace('K','').replace('$',''))
+minus_dollar_sign = salary.apply(lambda x: x.replace('K','').replace('$',''))
 
-min_hr = minus_Kd.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary:',''))
+del_salary = minus_dollar_sign.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary:',''))
 
-df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0]))
-df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
+df['min_salary'] = del_salary.apply(lambda x: int(x.split('-')[0]))
+df['max_salary'] = del_salary.apply(lambda x: int(x.split('-')[1]))
 df['avg_salary'] = (df.min_salary+df.max_salary)/2
 
 #Company name text only
